26b85de5f4cf9a106c27f88430df9b4954f09e22
Petrukhin <dartmol2300@gmail.com>
2024-11-05 12:54:51 +0300

    introduce way to request multiple asms (works synchronously for now)

--- jacodb-api-net/src/main/kotlin/org/jacodb/api/net/Main.kt
+++ jacodb-api-net/src/main/kotlin/org/jacodb/api/net/Main.kt
@@ -16,95 +16,12 @@
 
 package org.jacodb.api.net
 
-import com.jetbrains.rd.framework.*
-import com.jetbrains.rd.util.catch
-import com.jetbrains.rd.util.lifetime.Lifetime
-import com.jetbrains.rd.util.lifetime.isAlive
-import com.jetbrains.rd.util.reactive.IScheduler
-import com.jetbrains.rd.util.threading.SingleThreadScheduler
-import org.example.ilinstances.IlInstance
-import org.jacodb.api.net.generated.models.Request
-import org.jacodb.api.net.generated.models.ilModel
-import org.jacodb.api.net.generated.models.ilSigModel
-import java.io.File
-import java.util.concurrent.ConcurrentLinkedQueue
-import java.util.concurrent.TimeUnit
+import org.jacodb.api.net.rdinfra.NetApiServer
 
-fun pumpCurrentThread(lifetime: Lifetime, initializationAction: (IScheduler) -> Unit) {
-    val actions = ConcurrentLinkedQueue<() -> Unit>()
-    val currentThread = Thread.currentThread()
-    val scheduler = object : IScheduler {
-        override val isActive: Boolean
-            get() = currentThread == Thread.currentThread()
-
-        override fun flush() {
-            while (true) {
-                val action = actions.poll() ?: return
-                if (lifetime.isAlive)
-                    catch { action() }
-            }
-        }
-
-        override fun queue(action: () -> Unit) {
-            if (lifetime.isAlive)
-                actions.add(action)
-        }
-    }
-
-    initializationAction(scheduler)
-
-    j@ while (lifetime.isAlive) {
-        val action = actions.poll()
-        if (action == null) {
-            Thread.yield()
-            continue@j
-        }
-        catch { action() }
-    }
-}
 
 fun main() {
-    val socketLifetimeDef = Lifetime.Eternal.createNested()
-    val lifetime = socketLifetimeDef.lifetime
-
-    val serializers = Serializers()
-    pumpCurrentThread(lifetime) { scheduler ->
-        val protocol = Protocol(
-            "Server",
-            serializers,
-            Identities(IdKind.Server),
-            scheduler,
-            SocketWire.Server(lifetime, scheduler, 8083, "Server"),
-            lifetime
-        )
-
-        val ilModel = protocol.ilModel;
-        val sigModel = ilModel.ilSigModel;
-        val (process, alive) = spawnDotNetProcess("/home/andrew/Documents/dotnet-tac/TACBuilder/bin/Debug/net8.0/linux-x64/")
-
-        sigModel.asmResponse.advise(lifetime) { response ->
-            response.forEach { dto ->
-                IlInstance.cache.put(dto)
-            }
-            response.forEach { dto -> IlInstance.cache.get(dto).attach() }
-            println("response deserialized")
-            if (process.isAlive)
-                process.destroy()
-            println("process destroyed ${process.isAlive}")
-        }
-
-        assert(alive)
-        sigModel.asmRequest.fire(Request("/home/andrew/Documents/dotnet-tac/TACBuilder.Tests/bin/Release/net8.0/linux-x64/publish/TACBuilder.Tests.dll"))
-    }
-}
-
-fun spawnDotNetProcess(exePath: String): Pair<Process, Boolean> {
-    val process = ProcessBuilder("./TACBuilder", "--rd")
-        .directory(File(exePath))
-        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-        .redirectError(ProcessBuilder.Redirect.INHERIT)
-        .start()
-    val alive = process.waitFor(1, TimeUnit.SECONDS)
-    println(".NET process spawned")
-    return Pair(process, alive)
+    val api = NetApiServer()
+    api.requestTestAsm()
+    api.requestRdAsm()
+    api.close()
 }
---
+++ jacodb-api-net/src/main/kotlin/org/jacodb/api/net/rdinfra/NetApiServer.kt
@@ -0,0 +1,140 @@
+/*
+ *  Copyright 2022 UnitTestBot contributors (utbot.org)
+ * <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.jacodb.api.net.rdinfra
+
+import com.jetbrains.rd.framework.*
+import com.jetbrains.rd.util.lifetime.Lifetime
+import com.jetbrains.rd.util.spinUntil
+import com.jetbrains.rd.util.threading.SynchronousScheduler
+import org.example.ilinstances.IlInstance
+import org.jacodb.api.net.generated.models.*
+import java.io.File
+import java.time.LocalTime
+import java.util.concurrent.TimeUnit
+import kotlin.system.exitProcess
+
+private const val SPINNING_TIMEOUT = 1000L
+
+
+fun logWithTime(message: String) {
+    println("${LocalTime.now()}: $message ")
+}
+
+
+class RdServer(port: Int, private val netExePath: String) {
+    private val lifetimeDef = Lifetime.Eternal.createNested()
+    protected val lifetime = lifetimeDef.lifetime
+    val scheduler = SynchronousScheduler//(lifetime, "Scheduler")
+    val protocol = Protocol(
+        "Server", Serializers(), Identities(IdKind.Server), scheduler,
+        SocketWire.Server(lifetime, scheduler, port, "Server"), lifetime
+    )
+    val instanceModel: IlModel
+        get() = protocol.ilModel
+    val signalModel: IlSigModel
+        get() = instanceModel.ilSigModel
+    private lateinit var netProcess: Process
+
+    fun queue(action: () -> Unit) {
+        scheduler.queue {
+            try {
+                action()
+            } catch (e: Throwable) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun before() {
+        logWithTime("Starting server")
+        netProcess = spawnDotNetProcess(netExePath)
+        if (netProcess.isAlive) logWithTime("dotnet process spawned") else exitProcess(1)
+    }
+
+    private fun start() {
+        logWithTime("registering callbacks")
+        queue {
+            signalModel.asmResponse.advise(lifetime) { response ->
+                synchronized(IlInstance.cache) {
+                    response.forEach { dto ->
+                        IlInstance.cache.put(dto)
+                    }
+                    response.forEach { dto -> IlInstance.cache.get(dto).attach() }
+                }
+                logWithTime("response deserialized")
+            }
+        }
+        logWithTime("after queue")
+    }
+
+    fun close() {
+        logWithTime("Spinning started")
+        spinUntil(SPINNING_TIMEOUT) { false }
+        logWithTime("Spinning finished")
+
+        lifetimeDef.terminate()
+        if (netProcess.isAlive)
+            netProcess.destroy()
+    }
+
+    fun run() {
+        logWithTime("Test run")
+        try {
+            before()
+            start()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+        logWithTime("left run")
+    }
+
+    private fun spawnDotNetProcess(exePath: String): Process {
+        val process = ProcessBuilder("./TACBuilder", "--rd")
+            .directory(File(exePath))
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .redirectError(ProcessBuilder.Redirect.INHERIT)
+            .start()
+        process.waitFor(1, TimeUnit.SECONDS)
+        return process
+    }
+}
+
+class NetApiServer {
+    private val server = RdServer(8083, "/home/andrew/Documents/dotnet-tac/TACBuilder/bin/Debug/net8.0/linux-x64/")
+
+    init {
+        server.run()
+    }
+
+    fun requestTestAsm() {
+        requestAsm("/home/andrew/Documents/dotnet-tac/TACBuilder.Tests/bin/Release/net8.0/linux-x64/publish/TACBuilder.Tests.dll")
+    }
+
+    fun requestRdAsm() {
+        requestAsm("/home/andrew/Documents/dotnet-tac/TACBuilder.Tests/bin/Release/net8.0/linux-x64/publish/JetBrains.RdFramework.dll")
+    }
+
+    private fun requestAsm(path: String) {
+        logWithTime("requesting asm $path")
+        server.queue { server.signalModel.asmRequest.fire(Request(path)) }
+        logWithTime("request sent")
+    }
+
+    fun close() {
+        server.close()
+    }
+}
